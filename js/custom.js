b7c5293270b489617fd58694f18b0da49739bc44
a11arjunyadav <yadavarjun0312@gmail.com>
2023-08-21 16:34:21 +0530

    typescript

---
+++ js/custom.js
@@ -0,0 +1,5 @@
+var name1 = 'arjun'; //decleration and initialization
+console.log(name1);
+var age; //declaration
+age = 25;
+console.log(age);
---
+++ js/custom.ts
@@ -0,0 +1,9 @@
+
+var name1:string = 'arjun'; //decleration and initialization
+
+console.log(name1);
+
+var age:number //declaration
+
+age=25;
+console.log(age);
